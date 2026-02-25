0fe453a0b5a835dbe1de34af2218d104d926ba39
retrozinndev <joaovodias@gmail.com>
2026-02-24 21:07:04 -0300

    :wrench: chore(scripts, data, package): fix resources dir, update styles, update sass package

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,7 +1,5 @@
 set -e
 
-socket_support=true
-
 while getopts o:r:e:hn args; do
     case "$args" in 
         o)
@@ -12,9 +10,6 @@ while getopts o:r:e:hn args; do
             ;;
         e)
             bin_target=${OPTARG}
-            ;;
-        n)
-            unset socket_support
             ;;
         ? | h)
             echo "\
@@ -27,7 +22,6 @@ help:
 
 options:
   -r \$file: gresource's target path (literal; kept in \$output; default: /usr/share/vibe/resources.gresource)
-  -n: disable socket communication support(use the slower remote instance communication)
   -o \$path: build output path (default: \`./build/release\`)
   -e: set desktop entry's executable target (literal; default: /usr/bin/vibe)
   -h: show this help message"
@@ -40,6 +34,6 @@ done
 bash ./scripts/build.sh -o "${outdir:-"./build/release"}" -b -r "${gresource_file:-"/usr/share/vibe/resources.gresource"}"
 
 echo "[info] making desktop entry"
-entry=`cat ./resources/io.github.retrozinndev.vibe.desktop`
+entry=`cat ./data/io.github.retrozinndev.vibe.desktop`
 bin_target=${bin_target:-"/usr/bin/vibe"}
 echo -n "${entry/'$VIBE_BINARY'/${bin_target/'$'/'\\\$'}}" > "${outdir:-"./build/release"}/io.github.retrozinndev.vibe.desktop"
